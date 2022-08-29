b195d957c8de767806339b0278a30c3130dac8e3
NadiaVorontsova <Vorontsovvaaa@gmail.com>
2022-08-30 00:48:03 +0300

    Implemented based on classes (a-tiny-js-world)

--- index.js
+++ index.js
@@ -1,68 +1,91 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    
-   Code repository: https://github.com/NadiaVorontsova/a-tiny-JS-world
-   Web app: https://nadiavorontsova.github.io/a-tiny-JS-world/
+   Code repository: https://github.com/NadiaVorontsova/a-tiny-JS-world-OOP
+   Web app: https://nadiavorontsova.github.io/a-tiny-JS-world-OOP/
    */
 
-// ======== OBJECTS DEFINITIONS ========
+// ======== CLASS DEFINITIONS ========
+class Inhabitant {
+   constructor(species, name, gender, saying, friend) {
+      this.species = species;
+      this.name = name;
+      this.gender = gender;
+      this.saying = saying;
+      this.friend = friend || 'no friend:(';
+   }
+   printInhabitant() {
+      return `I am a <strong>${this.species}</strong>; My name is <strong>${this.name}</strong>; I am a <strong>${this.gender}</strong>;
+      I say: <strong>'${this.saying}'</strong>; My friends: <strong>${this.friend}</strong>`;
+   }
+}
 
-const dog = {
-   species: 'dog',
-   name: 'Ben',
-   gender: 'male',
-   legs: 4,
-   hands: 0,
-   saying: 'woof-woof'
-};
+class Human extends Inhabitant {
+   constructor(name, gender, saying, friend) {
+      super('human', name, gender, saying, friend);
+      this.legs = 2;
+      this.hands = 2;
+   }
+   printHuman() {
+      return `${this.printInhabitant()}; I have <strong>${this.hands} hands</strong> and <strong>${this.legs} legs</strong>;`;
+   }
+}
 
-const cat = {
-   species: 'cat',
-   name: 'Sarah',
-   gender: 'female',
-   legs: 4,
-   hands: 0,
-   saying: 'meow^^'
-};
+class Man extends Human {
+   constructor(name, gender, friend) {
+      super(name, gender, 'Hello everybody!', friend);
+   }
+}
 
-const man = {
-   species: 'human',
-   name: 'Yura',
-   gender: 'male',
-   legs: 2,
-   hands: 2,
-   saying: 'Hello everybody!',
-   friends: 'Oksana'
-};
+class Woman extends Human {
+   constructor(name, gender, friend) {
+      super(name, gender, 'Hello my friend!', friend);
+   }
+}
 
-const woman = {
-   species: 'human',
-   name: 'Oksana',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: 'Hello my friend!',
-   friends: 'Yura'
-};
+class Animal extends Inhabitant {
+   constructor(species, name, gender, saying, friend) {
+      super(species, name, gender, saying, friend);
+      this.paws = 4;
+      this.tail = 1;
+   }
+   printAnimal() {
+      return `${this.printInhabitant()}; I have <strong>${this.paws} paws</strong> and <strong>${this.tail} tail</strong>;`;
+   }
+}
 
-const catWoman = {
-   species: 'cat-human',
-   name: 'Halle Berry',
-   gender: 'female',
-   legs: 2,
-   hands: 2,
-   saying: cat.saying
-};
+class Dog extends Animal {
+   constructor(name, gender, friend) {
+      super('dog', name, gender, 'woof-woof', friend);
+   }
+}
+
+class Cat extends Animal {
+   constructor(name, gender, friend) {
+      super('cat', name, gender, 'meow^^', friend);
+   }
+}
+
+class CatWoman extends Cat {
+   constructor(name, gender, friend) {
+      super(name, gender, friend);
+      this.species = 'cat-human';
+   }
+}
 
 // ======== OUTPUT ========
 
-const inhabitancy = [dog, cat, man, woman, catWoman];
+const outputElement = document.createElement('div');
+const mainBlock = document.querySelector('main');
+mainBlock.appendChild(outputElement);
 
-function checkFriends(inhabitant) {
-   return inhabitant.friends || 'no friends';
-}
+const dog = new Dog('Ben', 'male');
+const cat = new Cat('Sarah', 'female', 'Halle Berry');
+const man = new Man('Yura', 'male', 'Oksana');
+const woman = new Woman('Oksana', 'female', 'Yura');
+const catWoman = new CatWoman('Halle Berry', 'female', 'Sarah');
 
-function printInhabitancy(inhabitant) {
-   return `${inhabitant.species}; ${inhabitant.name}; ${inhabitant.gender}; ${inhabitant.legs}; ${inhabitant.hands}; ${inhabitant.saying}; ${checkFriends(inhabitant)};`;
-}
+const animals = [dog, cat, catWoman];
+animals.map(animal => outputElement.insertAdjacentHTML('beforeend',animal.printAnimal()+'<br>'));
 
-inhabitancy.map((inhabitant) => print(printInhabitancy(inhabitant), 'div'));
+const people = [man, woman];
+people.map(human => outputElement.insertAdjacentHTML('beforeend',human.printHuman()+'<br>'));
